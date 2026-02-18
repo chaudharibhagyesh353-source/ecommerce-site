40d81df63ba9058ddc43845a4f5370102310d134
Bhagyesh Chaudhari <chaudharibhagyesh353@gmail.com>
2026-02-18 23:17:22 +0530

    Updated allowed hosts for render

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.getenv("DEBUG") == "True"
 ALLOWED_HOSTS = [
      "127.0.0.1",
     "localhost",
-    "ecommerce-site-cscm.onrender.com",
+    ".onrender.com",
 ]
 
 # -------------------------------
