ee368c4179f1884cdd1d9a7d58493dfe9fd66b67
Bhagyesh Chaudhari <chaudharibhagyesh353@gmail.com>
2026-02-22 10:50:06 +0530

    Fix sitemap domain properly

--- shop/sitemaps.py
+++ shop/sitemaps.py
@@ -2,14 +2,12 @@ from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 from .models import Product
 
-
 class ProductSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.8
-    protocol = "https"
 
     def items(self):
         return Product.objects.all()
 
     def location(self, obj):
-        return reverse("product_detail", args=[obj.slug])
+        return reverse("product_detail", kwargs={"slug": obj.slug})
